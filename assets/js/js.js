1473d1c7703d20d6c26e4f9195656adeb655db82
aow3xm <phamminh.aow3@gmail.com>
2023-11-28 00:16:21 +0700

    fixed resize window header

--- assets/js/js.js
+++ assets/js/js.js
@@ -4,3 +4,14 @@ let body = document.body;
 themeSwitch.addEventListener('change', function() {
     body.classList.toggle('dark-mode');
 });
+
+// tắt show nav khi resize trình duyệt
+window.addEventListener('resize', function() {
+    var header = document.querySelector('header#site-header')
+    var nav = document.getElementById('navbarSupportedContent');
+    var windowWidth = window.innerWidth;
+    if (windowWidth >= 768) {
+      header.classList.remove('expanded');
+      nav.classList.remove('show');
+    }
+  });
